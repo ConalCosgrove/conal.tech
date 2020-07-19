6588c1e53859478537d72f80f39908231323cedf
Conal Cosgrove <cosgroco@tcd.ie>
2020-07-19 04:40:32 +0100

    try api fix

--- src/data/fetchArticleList.ts
+++ src/data/fetchArticleList.ts
@@ -3,9 +3,7 @@ import { CardObject } from '../components/OptionCard';
 const {api} = config;
 
 export default async function fetchArticleList(): Promise<CardObject[]> {
-  console.log(api)
-  const articles = await fetch(`${api}/articles`);
-  console.log(articles)
+  const articles = await fetch(`${process.env.REACT_APP_}/articles`);
   const data = await articles.json();
   return data;
 }
