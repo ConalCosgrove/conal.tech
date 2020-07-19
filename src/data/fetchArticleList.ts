0233510736d1404efd80eb4256f12b2e6bdfa6e5
Conal Cosgrove <cosgroco@tcd.ie>
2020-07-19 04:45:06 +0100

    typo

--- src/data/fetchArticleList.ts
+++ src/data/fetchArticleList.ts
@@ -3,7 +3,7 @@ import { CardObject } from '../components/OptionCard';
 const {api} = config;
 
 export default async function fetchArticleList(): Promise<CardObject[]> {
-  const articles = await fetch(`${process.env.REACT_APP_}/articles`);
+  const articles = await fetch(`${process.env.REACT_APP_API_URL}/articles`);
   const data = await articles.json();
   return data;
 }
